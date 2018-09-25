0d7ce61e82d181afea90fb2a94dd72297f994213
Matheusbks <38140575+Matheusbks@users.noreply.github.com>
2018-09-25 15:22:31 -0300

    Alterações em OPrtunidades

--- WpOportunidades/Program.cs
+++ WpOportunidades/Program.cs
@@ -19,6 +19,7 @@ namespace WpOportunidades
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5330");
     }
 }
